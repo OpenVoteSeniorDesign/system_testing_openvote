4313ec35f0e2017957bc48f506211ca50b4f6495
Reed Cozart <rcozart@utexas.edu>
2015-11-16 14:24:06 -0600

    got the first complete system test working

--- src/systemtests/SingleSystemTest.java
+++ src/systemtests/SingleSystemTest.java
@@ -2,34 +2,93 @@ package systemtests;
 
 import static org.junit.Assert.*;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SingleSystemTest {
 	private static WebDriver wd;
+	private static String web_address = "http://openvote-poc.appspot.com/";
 
-	@BeforeClass public static void oneTimeSetup(){
+	@BeforeClass
+	public static void oneTimeSetup() {
 		wd = new FirefoxDriver();
-		wd.get("localhost:8888/login.jsp");
+		wd.get(web_address);
+		
+
+		
 	}
 
-	@Test public void t0() { 
-		// execute the test <x=four, y=four, z=four, computeButton=click() > and check the output message is correct
-		wd.navigate().refresh();
-		WebElement we = wd.findElement(By.id("login"));
-		we.sendKeys("login");
-		we = wd.findElement(By.id("password"));
-		we.sendKeys("password");
-		we = wd.findElement(By.id("btn_login"));
+	@Test public void t0(){
+		WebElement we = wd.findElement(By.id("btn_continue_not_admin"));
 		we.click();
-		WebDriverWait wdw = new WebDriverWait(wd, 5);
-		WebElement greeting = wd.findElement(By.className("panel-heading"));
-		String output = greeting.getText(); // read the output text
-		assertEquals("Cast Your Vote!", output);
+		
+		we = wd.findElement(By.id("Email"));
+		we.sendKeys("reed.cozart@gmail.com");
+		we = wd.findElement(By.id("next"));
+		we.click();
+		WebDriverWait wait = new WebDriverWait(wd, 5);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Passwd")));
+		
+		we = wd.findElement(By.id("Passwd"));
+		we.sendKeys("g3931eATX");
+		we = wd.findElement(By.id("signIn"));
+		we.click();
+		
+		wd.findElement(By.id("btn_continue_admin")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn_Dory")));
+		we = wd.findElement(By.id("btn_Dory"));
+		we.click();
+		
+		wd.findElement(By.id("btn_submit")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("id_num")));
+		
+		WebElement id = wd.findElement(By.id("id_num"));
+		String id_str = id.getText();
+		System.out.println(id_str);
+		
+		WebElement candidate = wd.findElement(By.id("candidate_name"));
+		String cand_str = candidate.getText();
+		assertEquals("Dory", cand_str);
+
+		
+		we = wd.findElement(By.id("btn_goodbye"));
+		we.click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn_gotoliveresults")));
+		
+		we = wd.findElement(By.id("btn_gotoliveresults"));
+		String btn_str = we.getAttribute("value");
+		assertEquals(btn_str, "Click here to go to Live Results!");
+		we.click();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn_refresh")));
+		
+		we = wd.findElement(By.id("btn_refresh"));
+		btn_str = we.getAttribute("value");
+		assertEquals(btn_str, "Refresh");
+		we = wd.findElement(By.id("btn_return_login"));
+		btn_str = we.getAttribute("value");
+		assertEquals(btn_str, "Return to Login");
+		
+		wd.get(web_address + "/viewSingleVote.jsp?votekey=" + id_str);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("id_entry")));
+
+		assertEquals(id_str, wd.findElement(By.id("id_entry")).getText());
+		assertEquals("Dory", wd.findElement(By.id("candidate_entry")).getText());
 	}
+
+	@AfterClass
+	public static void oneTimeTearDown() {
+		wd.close();
+	}
+
 }
